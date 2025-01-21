3083d1d68817c4197315bd569f1c80ac5ab44572
JacksonRoodman <jacksonroodman@gmail.com>
2025-01-20 17:05:55 -0800

    bees

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 function enableSound(){
     var audio = document.getElementById("music");
     audio.play()
+    var credit = document.getElementById("music-credit");
+    credit.style.display = 'block';
 }
